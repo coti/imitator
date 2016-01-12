b88d2cacdcbef704b7aa8fbdd1da362cd8e1d8de
Camille Coti <camille.coti@lipn.univ-paris13.fr>
2016-01-12 18:19:27 +0100

    Nicer prints

--- scripts/check_equivalence/z3check.py
+++ scripts/check_equivalence/z3check.py
@@ -15,6 +15,12 @@ def printUsage( argv ):
         print "\t", argv[0], "<file1> <file2>"
         return
 
+def printParameterSpace( param ):
+        print " = PARAMETER SPACE ="
+        for p in param:
+                print p[0], ":", p[1]
+        return
+
 def tabToSpace( param ):
         space = "z3.And("
         s_string = []
@@ -49,8 +55,10 @@ def getVariableNames( filename ):
                 tab.append( p[0] )
         return tab
 
-def getParameterSpace( filename ):
+def getParameterSpace( filename, display = False ):
         param = constraints.getParameters( filename )
+        if display:
+                printParameterSpace( param )
         s = tabToSpace( param )
         return s
 
@@ -58,7 +66,6 @@ def declareRealVariables( filename ):
         tab = getVariableNames( filename )
         symb = []
         for t in tab:
-                print "Declare", t
                 s = z3.Real( t )
                 symb.append( s )
                 globals()[t] = s
@@ -93,28 +100,28 @@ def check_inclusion_witness( f, g ):
         gormula = eval( Not( g ) )
         solv.append( gormula )
         if solv.check() == z3.sat:
-                print solv.model()
+                print "Found point:", solv.model()
         return
 
 def main( argv ):
         z3carto = getTableOfConstraints( argv[1] )
         vanilla = getTableOfConstraints( argv[2] )
-        space = getParameterSpace( argv[1] )
+        space = getParameterSpace( argv[1], True )
         symbols = declareRealVariables( argv[1] )
-
+        
         print " =-=-= CARTOZ3 =-=-="
-        print check_equiv( applyOr( z3carto ), space )
-        print check_inclusion( applyOr( z3carto ), space)
+        print "Equivalence:", check_equiv( applyOr( z3carto ), space )
+        print "Inclusion:  ", check_inclusion( applyOr( z3carto ), space)
         check_inclusion_witness( applyOr( z3carto ), space )
 
         print " =-=-= VANILLA =-=-="
-        print check_equiv( applyOr( vanilla ), space )
-        print check_inclusion( applyOr( vanilla ), space)
+        print "Equivalence:", check_equiv( applyOr( vanilla ), space )
+        print "Inclusion:  ", check_inclusion( applyOr( vanilla ), space)
         check_inclusion_witness( applyOr( vanilla ), space )
         return
 
 if __name__ == "__main__":
-    if len( sys.argv ) < 2:
+    if len( sys.argv ) < 3:
         printUsage( sys.argv )
         exit( -1 )
     main( sys.argv )
